d446157c6e0bf52896cd6613b84bb2edcea16b83
lebowvsky <lebowvsky@gmail.com>
2020-06-12 10:49:34 +0200

    Styling GlobalStyle Done

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <ProjectRouter />
         <GlobalStyle />
+        <ProjectRouter />
       </div>
     </ThemeProvider>
   );
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
+import Museo from '../fonts/Museo-500.otf';
+import MuseoBold from '../fonts/Museo-900.otf';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  @font-face {
+    font-family: 'MuseoNormal';
+    src: url(${Museo});
+  }
 
+  @font-face {
+    font-family: 'MuseoBold';
+    src: url(${MuseoBold});
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'MuseoNormal';
   }
 `;
 export default GlobalStyle;
