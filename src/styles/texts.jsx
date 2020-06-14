24f155f6c5ae765a13f5810c246cd442fc2c801c
Brice Le Gallo <lebowvsky@gmail.com>
2020-06-14 23:59:40 +0200

    Login Page is created with everything on it

---
+++ src/styles/texts.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  font-family: 'MuseoBold';
+  font-size: 2.5rem;
+  margin: 3vh;
+  color: ${(props) => props.theme.grey};
+`;
+
+export const H2 = styled.h2`
+  font-size: 1.5rem;
+`;
+
+export const TextParagraph = styled.p`
+  color: ${(props) => props.grey && props.theme.grey};
+`;
