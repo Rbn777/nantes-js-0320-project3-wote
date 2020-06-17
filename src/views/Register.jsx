fdf70074d767ee562827f47a4c152d8da8f8b165
Tobii777 <robin.dorion@gmail.com>
2020-06-17 17:30:04 +0200

    Adding Toastifies for errors and success in registration

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import styled from 'styled-components';
+
+import { ToastContainer, toast } from 'react-toastify';
 
 import FormCpnt from '../components/FormCpnt';
 import InputCpnt from '../components/InputCpnt';
@@ -15,26 +18,88 @@ const RegisterContainer = styled(FlexDiv)`
 `;
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [termsOfUseCheck, setTermsOfUseCheck] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleNewPassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPassword = (e) => {
+    setPasswordCheck(e.target.value);
+  };
+
+  const handleTermsOfUseCheck = () => {
+    setTermsOfUseCheck(!termsOfUseCheck);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password || !passwordCheck || !termsOfUseCheck) {
+      toast.warn(`Tous les champs doivent être renseignés...`, {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else if (password !== passwordCheck) {
+      toast.error('Les mots de passe doivent correspondre', {});
+    } else if (!termsOfUseCheck) {
+      toast.error("Les conditions d'utilisation doivent être approuvées", {});
+    } else {
+      axios
+        .post(`http://localhost:4545/api/users`, { email, password })
+        .then((response) => response.data)
+        .then((res) => {
+          toast.success(`L'utilisateur a été ajouté avec l'ID ${res.id} !`, {});
+        })
+        .catch((error) => {
+          toast.success(
+            `Erreur lors de l'ajout d'un utilisateur : ${error.message}`,
+            {}
+          );
+        });
+    }
+  };
+
   return (
     <RegisterContainer column>
-      <FormCpnt>
+      <FormCpnt submitFuncToPass={handleSubmit}>
         <InputCpnt
           labelText="Email"
           inputType="email"
           nameForInput="userLogin"
           inputPlaceHolder="Email de connexion..."
+          value={email}
+          onChangeFunc={handleEmail}
+          inputRequired
         />
         <InputCpnt
           labelText="Mot de passe"
           inputType="password"
           nameForInput="userPassword"
           inputPlaceHolder="Mot de passe..."
+          value={password}
+          onChangeFunc={handleNewPassword}
+          inputRequired
         />
         <InputCpnt
           labelText="Confirmation du mot de passe"
           inputType="password"
           nameForInput="userPasswordCheck"
           inputPlaceHolder="Mot de passe..."
+          value={passwordCheck}
+          onChangeFunc={handleConfirmPassword}
+          inputRequired
         />
         <InputCpnt
           labelText={
@@ -47,12 +112,26 @@ const Register = () => {
           }
           inputType="checkbox"
           nameForInput="termsOfUseCheckbox"
+          value={termsOfUseCheck}
+          onChangeFunc={handleTermsOfUseCheck}
+          inputRequired
           rowReverse
         />
         <Button buttonType="submit" greenBg>
           Enregistrer
         </Button>
       </FormCpnt>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </RegisterContainer>
   );
 };
