ed4fdf66f52b68acee962f1355f3cbecc60c4f8b
Code-Hello <m.tuto@protonmail.com>
2020-06-29 14:26:35 +0200

    cursor started with material ui

--- src/views/Convictions.jsx
+++ src/views/Convictions.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cursor from '../components/Cursor';
 
 const Convictions = () => {
   return (
     <div>
       <h2>Page Convictions</h2>
+      <Cursor color="#92be1f">Titre cursor</Cursor>
     </div>
   );
 };
